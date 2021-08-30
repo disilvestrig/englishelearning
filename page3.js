97863da9eb924b27e6e1e568895a2897932d2b9e
Giulio Di Silvestri <42960623+disilvestrig@users.noreply.github.com>
2021-08-30 18:28:29 +0200

    Update page3.js

--- page3.js
+++ page3.js
@@ -1,15 +1,12 @@
 form1 = document.getElementById("b1")
 sol1=["turned","down","the"]
-sol2=["Why don't you come to visit us on Sunday?","Why do not you come to visit us on Sunday?"]
-sol3=["Let's take a taxi. I don't want us to be late.","Let us take a taxi.I do not want us to be late","Let us take a taxi.I don't want us to be late","Let's take a taxi. I do not want us to be late"]
-sol4=["Did Ron go upstairs to study or to play?"]
-sol5=["Hurry up to finish your homework."]
-sol6=["Come and sit in the garden."]
-sol7=["Let's go play soccer this afternoon.","Let's go to play soccer this afternoon.","Let us go to play soccer this afternoon.","Let us go play soccer this afternoon."]
-sol8=["Meryl doesn't know whether to accept her boss's invitation or not.","Meryl does not know whether to accept her boss's invitation or not."]
-sol9=["I have told you many times not to read my emails.","I've told you many times not to read my emails."]
-sol10=["Do you want me to turn on the light?"]
-const solutions = [sol1,sol2,sol3,sol4,sol5,sol6,sol7,sol8,sol9,sol10]
+sol2=["make","up","for"]
+sol3=["held","up"]
+sol4=["made","her","mind","up"]
+sol5=["let","his","family","down"]
+sol6=["had","not","turned","up"]
+sol7=["see","mary","off"]
+const solutions = [sol1,sol2,sol3,sol4,sol5,sol6,sol7]
 
 const correct = () => {
     let mark = 0
@@ -22,14 +19,24 @@ const correct = () => {
     let s5 = document.exercise.s5.value
     let s6 = document.exercise.s6.value
     let s7 = document.exercise.s7.value
-    let s8 = document.exercise.s8.value
-    let s9 = document.exercise.s9.value
-    let s10 = document.exercise.s10.value
-    let answers1 = [s1,s2,s3,s4,s5,s6,s7,s8,s9,s10]
+    let answers1 = [s1,s2,s3,s4,s5,s6,s7]
     let answers2 = answers1.map ( e => e.trim())
+    let answers4 = answers2.map ( e => e.toLowerCase())
     let anwers3 
-    let answers = answers2.map( e => e.split(' '))
+
+    let answers5 = answers4.map( e => e.split(' '))
+    console.log(answers4)
+    let answers = answers5.map( e => {
+        for ( let x = 0;x<e.length;x++){
+            if (e[x] == "hadn't"){
+                e[x] = "had"
+                e.splice(x+1,0,"not")
+            }
+        }
+        return e
+    }) 
     let check = 0
+    let l = 0
 
     for(let i = 0;i<answers.length;i++){
         check = 0
@@ -38,15 +45,15 @@ const correct = () => {
             return e != "" 
             })
         for(let j = 0;j<solutions[i].length;j++){
-            console.log(answers3)
-            if (solutions[i].length!=answers[i].length){
+            l = j
+            if (solutions[i].length!=answers[i].length && solutions[i].length!=answers[i].length-1 ){
                 break
             }
-            if (solutions[i][j] != answers3[j]){
-                
+            if (solutions[i][j] != answers3[l] ){
                 break
             }
             check ++
+
         }
         if (check == solutions[i].length){
             mark++
@@ -59,7 +66,7 @@ const correct = () => {
             mistakes.push("Wrong answer : "+answers[i]+" The solution is : "+solutions[i][0])
         }
     }
-    for( let k = 0;k< 10;k++){
+    for( let k = 0;k< 7;k++){
         if (mistakes[k] != "Right answer"){
             document.getElementById("sol"+String(k+1)).innerHTML = "<button onclick=ss"+(k+1)+"()>Show solutions</button> <img src='https://icon2.cleanpng.com/20190228/bre/kisspng-red-x-stock-photography-image-letter-portable-netw-5c7878899969a0.1754724515513990496284.jpg' height=20vh width=20vh>"
         }
@@ -76,38 +83,26 @@ function ss1 () {
 }
 
 function ss2 () {
-    document.getElementById("sol2").innerHTML= sol2[0]
+    document.getElementById("sol2").innerHTML= sol2[0]+" "+sol2[1]+" "+sol2[2]
 }
 
 function ss3 () {
-    document.getElementById("sol3").innerHTML= sol3[0]
+    document.getElementById("sol3").innerHTML= sol3[0]+" "+sol3[1]
 }
 
 function ss4 () {
-    document.getElementById("sol4").innerHTML= sol4[0]
+    document.getElementById("sol4").innerHTML= sol4[0]+" "+sol4[1]+" "+sol4[2]+" "+sol4[3]
 }
 
 function ss5 () {
-    document.getElementById("sol5").innerHTML= sol5[0]
+    document.getElementById("sol5").innerHTML= sol5[0]+" "+sol5[1]+" "+sol5[2]+" "+sol5[3]
 }
 
 function ss6 () {
-    document.getElementById("sol6").innerHTML= sol6[0]
+    document.getElementById("sol6").innerHTML= sol6[0]+" "+sol6[1]+" "+sol6[2]+" "+sol6[3]
 }
 
 function ss7 () {
-    document.getElementById("sol7").innerHTML= sol7[0]
-}
-
-function ss8 () {
-    document.getElementById("sol8").innerHTML= sol8[0]
-}
-
-function ss9 () {
-    document.getElementById("sol9").innerHTML= sol9[0]
-}
-
-function ss10 () {
-    document.getElementById("sol10").innerHTML= sol10[0]
+    document.getElementById("sol7").innerHTML= sol7[0]+" "+sol7[1]+" "+sol7[2]
 }
 
